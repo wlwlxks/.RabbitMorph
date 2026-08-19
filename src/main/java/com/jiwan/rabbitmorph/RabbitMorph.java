f6bf968680d1199bd08fa15787690d58325b8938
Limjiwan <jiwaunjim0306@gmail.com>
2026-08-19 15:42:20 +0900

    Initial Release v1.0.0 - Fix model facing, preview clarity/upright orientation, non-overlapping GUI, and modular tab architecture

--- src/main/java/com/jiwan/rabbitmorph/RabbitMorph.java
+++ src/main/java/com/jiwan/rabbitmorph/RabbitMorph.java
@@ -21,7 +21,7 @@ public class RabbitMorph {
 
     public static final String MODID = "rabbitmorph";
     public static final String NAME = "RabbitMorph";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.0.0";
 
     @Instance(MODID)
     public static RabbitMorph instance;
